3311b9697cb7b1d94fb7e451dfa5e41c8418f4d5
Mehedi Hasan <77448606+fnnaeem1881@users.noreply.github.com>
2022-01-30 12:32:30 +0600

    vue router init
    vue router init

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,10 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
